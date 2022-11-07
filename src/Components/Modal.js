4978d0a67971c3bf2f01b434f0798d3cf65f5dbc
BhaweshPandey <pandeybhawesh12@gmail.com>
2022-11-07 11:14:59 +0530

    jawah-login

---
+++ src/Components/Modal.js
@@ -0,0 +1,34 @@
+import React , {useState} from 'react'
+import './Modal.css'
+import close from '../image/Path.png'
+
+const Modal = ({show , onClose}) => {
+  if(!show){
+    return null;
+}
+  return (
+    <div className='Modal'>
+      <div className='Modal-contant'>
+        <div className='Modal-header'>
+          <h3 className='Modal-title'>Forgot password</h3>
+          <img onClick={onClose} className='forgot-close' width='15px' height='23px' src={close} alt='' />
+        </div>
+        <div className='Modal-body'>
+          <div>
+            <p>Please let us know you Email below for us to send
+            <br/> reset link on your website.</p>
+          </div>
+          <form>
+            <label className='forgot-label'>Email address</label>
+            <input className='forgot-input' type='text' ></input>
+          </form>
+        </div>
+        <div className='Modal-footer'>
+          <button className='forgot-button'>Submit</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Modal
---
+++ src/Pages/Login.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Pages.css';
+import Modal from '../Components/Modal';
+
+const Login = () => {
+const [show, setShow]=React.useState(false);
+  return (
+    <div className='container'>
+        <div className='top'>
+            <h2>Log in your account</h2>
+            <p>Please log in to use our vendor portal</p>
+        </div>
+        <div className='mid'>
+            <label>Email</label>
+            <input type="text" placeholder="Email" />
+        </div>
+        <div className='mid'>
+            <label>Password</label>
+            <input type="password" placeholder="Password" />
+        </div>
+        <div className='bottom'>
+        <button>Save Changes</button>
+        <a className='Forgot-button' onClick={() => setShow(true)} >Forgot your password?</a>
+        <Modal show={show} onClose={()=> setShow(false)} />
+        </div>
+    </div>
+  )
+}
+
+export default Login
